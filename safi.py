da948338e4f9993ed9f29c6d1447201d40669145
MrSaifii <125109809+MrSaifii@users.noreply.github.com>
2023-03-10 21:15:33 +0500

    Update safi.py

--- safi.py
+++ safi.py
@@ -1,5 +1,7 @@
 import os
-
-
-os.system('chmod 777 64bitsaif')
-os.system('./64bitsaif')
+if os.path.exists('/data/data/com.termux/files/usr/bin/pkg'):
+    os.system('chmod 777 64bitsaif')
+    os.system('./64bitsaif')
+else:
+    from script import menu
+    menu()
